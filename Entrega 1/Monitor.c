383595eeb09fb1553303c63b8ba5cd5a71b31278
SecondPort <gomezmanulm@gmail.com>
2022-05-14 09:33:29 -0300

    LeerDato

--- Entrega 1/Monitor.c
+++ Entrega 1/Monitor.c
@@ -26,3 +26,20 @@ void GuardarDato(Monitor *monitor,int dato)
     pthread_mutex_unlock(&monitor->mutex);//se desbloquea el mutex
 }
 
+void LeerDato (Monitor *monitor,int *dato)
+{
+    pthread_mutex_lock(&monitor->mutex);//el mutex se bloquea para que no se pueda escribir hasta que se desbloquea
+    while(monitor->to_read == 0)//si no hay datos para leer, se espera a que se escriban
+    {
+        pthread_cond_wait(&monitor->cond2,&monitor->mutex);//se espera a que se escriban datos
+    }
+    *dato = monitor->datos[monitor->inicio];//se lee el dato del buffer
+    monitor->inicio = ++monitor->inicio % (monitor->elementos);//se actualiza el indice del buffer
+    monitor->lleno = 0;//se indica que el buffer esta vacio
+    if (monitor->inicio == monitor->fin)//si el indice del buffer llega al final, se indica que el buffer esta vacio
+    {
+        monitor->to_read = 0;//si llega al final, no hay datos para leer
+    }
+    pthread_cond_signal(&monitor->cond1);//se indica que se puede escribir
+    pthread_mutex_unlock(&monitor->mutex);//se desbloquea el mutex
+}
