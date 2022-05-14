00e14c5a6cce49eb441c466194229532c628edbe
SecondPort <gomezmanulm@gmail.com>
2022-05-14 09:37:53 -0300

    comentarios

--- Entrega 1/Monitor.h
+++ Entrega 1/Monitor.h
@@ -3,15 +3,15 @@
 
 typedef struct
 {
-    int elementos;
-    int datos[5];
-    int lleno;
-    int inicio,fin;
-    int to_read;
-    pthread_cond_t cond1,cond2;
-    pthread_condattr_t condattr1,condattr2;
-    pthread_mutex_t mutex;
-    pthread_mutexattr_t mutexattr;
+    int elementos;//cantidad de elementos del buffer
+    int datos[5];//buffer
+    int lleno;//indica si el buffer esta lleno
+    int inicio,fin;//indices del buffer
+    int to_read;//indica si hay datos para leer
+    pthread_cond_t cond1,cond2;//señales para indicar que se puede escribir y leer
+    pthread_condattr_t condattr1,condattr2;//atributos de las señales
+    pthread_mutex_t mutex;//mutex para bloquear el acceso a los datos
+    pthread_mutexattr_t mutexattr;//atributos del mutex
 
 }Monitor;
 
